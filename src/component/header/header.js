5f331897e1f29940e6674424ff4f69cd870a579c
unknown <zahidmd1996@gmail.com>
2022-10-16 02:15:23 +0530

    header

---
+++ src/component/header/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <>
+            <div className="row justify-content-between mt-5">
+                <div className="col-lg-6">
+                    <p>Hello world</p>
+                </div>
+                <div className="col-lg-4">
+                    <p>helllo world</p>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import Card from '../component/card/card'
+import Header from '../component/header/header'
 import Navbar from '../component/navbar/navbar'
 
 export default function Home() {
     return (
         <>
-            <div className="container-fluid">
+            <div className="container-fluid p-0">
                <Navbar/> 
             </div>
             <div className="container">
-                <div className="row">
-                    <div className="col-lg-4">
-                        <Card/>
-                    </div>
-                </div>
+                <Header/>
             </div>
         </>
     )
